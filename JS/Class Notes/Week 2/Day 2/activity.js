35c5b92b29c5ad5883d2a81e148961f8335b163e
Woody200103 <hazelwooddavid7@gmail.com>
2025-05-27 21:29:35 -0400

    added comments to all my pages

--- JS/Class Notes/Week 2/Day 2/activity.js
+++ JS/Class Notes/Week 2/Day 2/activity.js
@@ -1,26 +1,66 @@
 // Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+let firstName = 'David';
+let lastName = 'Hazelwood';
+let country = 'USA';
+let city = 'Bean Station';
+let age = 36;
+let isMarried = false;
+let year = 2025;
 
-// Check if type of '10' is equal to 10
+console.log(typeof firstName); // string
+console.log(typeof lastName); // string
+console.log(typeof country);  // string
+console.log(typeof city); // string
+console.log(typeof age); // number 
+console.log(isMarried);
+console.log(year); 
+
+// Check if type of '10' is equal to 10 //
+console.log(typeof '10'); // string
+console.log(typeof 10); // number
+ console.log(typeof '10' === typeof 10); // false, because '10' is a string and 10 is a number
 
 // Check if parseInt('9.8') is equal to 10
+console.log(parseInt('9.8')); // 9
+console.log(parseInt('9.8') === 10); // false, because parseInt('9.8') returns 9
 
-// Boolean value is either true or false.
+// Boolean value is either true or false. 
+
 
 // Write three JavaScript statement which provide truthy value.
+let truthyValue1 = 1; // non-zero number
+let truthyValue2 = 'Hello'; // non-empty string
+let truthyValue3 = {}; // non-empty object
+
 // Write three JavaScript statement which provide falsy value.
+let falsyValue1 = 0; // zero
+let falsyValue2 = ''; // empty string
+let falsyValue3 = null; // null value
 // Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
 
 // 4 > 3
+true; console.log(4 > 3); // true  
 // 4 >= 3
+true; console.log(4 >= 3); // true
 // 4 < 3
+false; console.log(4 < 3); // false
 // 4 <= 3
+false; console.log(4 <= 3); // false
 // 4 == 4
+true; console.log(4 == 4); // true
 // 4 === 4
+true; console.log(4 === 4); // true
 // 4 != 4
+false; console.log(4 != 4); // false
 // 4 !== 4
+false; console.log(4 !== 4); // false
 // 4 != '4'
+false; console.log(4 != '4'); // false, because == checks value only
 // 4 == '4'
+true; console.log(4 == '4'); // true, because == checks value only
 // 4 === '4'
+false; console.log(4 === '4'); // false, because === checks both value and type
+
 // Find the length of python and jargon and make a falsy comparison statement.
 // Figure out the result of the following expressions first without using console.log(). After you decide the result confirm it by using console.log()
 
