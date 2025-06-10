366514246e0946e2f34376f2a13fdae05975de86
Woody200103 <hazelwooddavid7@gmail.com>
2025-06-09 21:05:20 -0400

    fizzbuzz activity

--- JS/Assignments/Week 2/fizzy/main.js
+++ JS/Assignments/Week 2/fizzy/main.js
@@ -1,8 +1,35 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+/*code must include 1 loop, if/else statements, fizz for multiples of 3, buzz for multiples of 5,fizzbuzz for multiples of 3 n 5, print numbers from 1-100 */
+function fizzy(n) {
+if (n % 3 === 0 && n % 5 === 0) {
+  return "FizzBuzz: " + n;
+} else if (n % 3 === 0){
+  return "Fizz: " + n;
+} else if (n % 5 === 0){
+  return "Buzz: " + n;
+} else {
+  return n;
+}
+}
 
+console.log(fizzy(22));
+console.log(fizzy(3));
+console.log(fizzy(100));console.log(fizzy(15));
 
+/*
+for (let i = 1; i <= 100; i++){
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}*/
 
 
 /**********************************
---
+++ JS/Dompractice.js
@@ -0,0 +1,9 @@
+// const allTitles = document.getByTagName("h1");
+
+
+//loop
+// for (initialization; condition; increment) {
+
+//initialization runs once
+//condition runs before each iteration  
+//update runs after each execution
